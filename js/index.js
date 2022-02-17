921d6e6b5c1ecd5676e64aaaf07ad693130a5cfc
Tohidul29 <tohidswfu@gmail.com>
2022-02-17 20:00:21 +0600

    full work completed

--- js/index.js
+++ js/index.js
@@ -1,9 +1,8 @@
-// function for counting total expense:
+// function for counting total expenses:
 function addTotalExpenses(food, rent, clothes) {
     const totalExpense = parseFloat(food) + parseFloat(rent) + parseFloat(clothes);
     return totalExpense;
 }
-
 
 // get all the input values from input id:
 const foodExpense = document.getElementById('food-amount');
@@ -12,12 +11,15 @@ const clothesExpense = document.getElementById('cloth-amount');
 const income = document.getElementById('income-amount');
 const save = document.getElementById('saving-number');
 
-
 // get all the amount texts from html tags:
-const balanceAmountText = document.getElementById('balance-amount');
-const savingAmountText = document.getElementById('saving-amount');
-const expenseAmountText = document.getElementById('expense-amount');
-const remainingAmountText = document.getElementById('remaining-amount');
+const balanceAmountNumber = document.getElementById('balance-amount');
+const savingAmountNumber = document.getElementById('saving-amount');
+const expenseAmountNumber = document.getElementById('expense-amount');
+const remainingAmountNumber = document.getElementById('remaining-amount');
+const totalExpenseText = document.getElementById('total-expense-text');
+const balanceAmountText = document.getElementById('balance-amount-text');
+const savingAmountText = document.getElementById('saving-amount-text');
+const remainingAmountText = document.getElementById('remaining-amount-text');
 
 
 // calculate button funtionality here:
@@ -26,61 +28,58 @@ document.getElementById('calculate').addEventListener('click', function () {
     rentExpenseValue = rentExpense.value;
     clothesExpenseValue = clothesExpense.value;
     incomeValue = income.value;
-    if (!(inputValidation(incomeValue) && inputValidation(foodExpenseValue) && inputValidation(rentExpenseValue) && inputValidation(clothesExpenseValue))) {
+    if (!(inputValidationChecking(incomeValue) && inputValidationChecking(foodExpenseValue) && inputValidationChecking(rentExpenseValue) && inputValidationChecking(clothesExpenseValue))) {
         return;
     }
 
     const expense = addTotalExpenses(foodExpenseValue, rentExpenseValue, clothesExpenseValue);
 
     if (expense > incomeValue) {
-        expenseAmountText.innerText = 'you spent more than your income';
+        totalExpenseText.innerText = 'Sorry, you spent more than your income !';
+        balanceAmountText.style.display = 'none';
+        totalExpenseText.style.color = 'rgba(197,46,54,0.85)';
     } else {
-        expenseAmountText.innerText = expense;
+        expenseAmountNumber.innerText = expense;
         balanceAmount = parseFloat(incomeValue) - parseFloat(expense);
-    } balanceAmountText.innerText = balanceAmount;
+        balanceAmountNumber.innerText = balanceAmount;
 
-    foodExpense.value = '';
-    rentExpense.value = '';
-    clothesExpense.value = '';
-    income.value = '';
+    }
 })
-
 
 // save button funtionality here:
 document.getElementById('save').addEventListener('click', function () {
-    foodExpenseValue = foodExpense.value;
-    rentExpenseValue = rentExpense.value;
-    clothesExpenseValue = clothesExpense.value;
     incomeValue = income.value;
     savingAmountValue = save.value;
-    // input validation
-    if (!(inputValidation(incomeValue) && inputValidation(foodExpenseValue) && inputValidation(rentExpenseValue) && inputValidation(clothesExpenseValue) && inputValidation(savingAmountValue))) {
-        savingAmountText.innerText = 'please give save input between (0 to 100)%';
+
+    if (!(inputValidationChecking(savingAmountValue))) {
         return;
     }
 
+    // input validation
     savingAmount = (parseFloat(incomeValue) * parseFloat(savingAmountValue)) / 100;
-    savingAmountText.innerText = savingAmount;
+    savingAmountNumber.innerText = savingAmount;
 
     // remaining balance:
     if (savingAmount < balanceAmount) {
         remainingBalance = parseFloat(balanceAmount) - parseFloat(savingAmount);
-        remainingAmountText.innerText = remainingBalance;
+        remainingAmountNumber.innerText = remainingBalance;
     } else {
-        remainingAmountText.innerText = 'saving amount can not be more than balance';
-    } 
-    save.value = '';
+        remainingAmountText.innerText = 'Sorry, saving amount can not be more than balance';
+        remainingAmountText.style.color = 'rgba(161,0,8,0.85)';
+    }
 })
 
-// validation of expense fields
-function inputValidation(inp) {
-    inp = parseInt(inp);
-    if (typeof inp == 'number' && inp >= 0) {
+// validation for all of the input fields:
+function inputValidationChecking(userInput) {
+    userInput = parseFloat(userInput);
+    if (typeof userInput == 'number' && userInput >= 0) {
         return true;
-    } else {
-        balanceAmountText.innerText = 'Invalid input !'; // setting invalid input
-        balanceAmountText.style.color = 'black';
-        expenseAmountText.innerText = '';
+    }
+    else {
+        //if invalid input:
+        totalExpenseText.innerText = 'Invalid or empty input !';
+        totalExpenseText.style.color = 'rgba(245,6,18,0.85)';
+        balanceAmountText.style.display = 'none';
         return false;
     }
 }
